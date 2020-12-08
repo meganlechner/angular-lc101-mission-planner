fba44a61b125551706957d01e7ee595b5b3ca83b
unknown <meganlechner@gmail.com>
2020-12-07 20:18:48 -0600

    Studio-Angular1Bonus

--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
